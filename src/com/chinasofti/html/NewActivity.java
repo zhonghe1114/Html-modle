7b9ba9587a1df764b99944a762a64cb470624604
钟何亮 <zhonghe1114@yahoo.cn>
2013-03-21 09:45:27 +0800

    zhong

---
+++ src/com/chinasofti/html/NewActivity.java
@@ -0,0 +1,16 @@
+package com.chinasofti.html;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.msg);
+	}
+
+}
